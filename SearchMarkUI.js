2d3a242091e7831e3a74a7817abbc434837ffc55
Miolav-v <57397518+Miolav-v@users.noreply.github.com>
2025-04-09 18:43:47 +0300

    Update SearchMarkUI.js

--- SearchMarkUI.js
+++ SearchMarkUI.js
@@ -1,158 +1,90 @@
-// SearchMarkUI.html: SearchMark front-end
-//
-// Copyright (C) 2010  Candy Yiu, and Akshay Dua
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// SearchMarkUI.js: SearchMark front-end
+// Copyright (C) 2010 Candy Yiu, and Akshay Dua
+// GNU General Public License
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+$(document).ready(function() {
+  // Устанавливаем слушатель сообщений от background.js
+  chrome.runtime.onMessage.addListener((result) => {
+    processSearchResult(result);
+  });
 
-// About:
-// Initialize communication channel between backend and UI.
-// No UI code should be added here. Change only if ABSOLUTELY
-// necessary.
-//
+  localStorage.setItem('uivisits', (parseInt(localStorage.getItem('uivisits') || 0) + 1));
 
-$(document).ready(
-    function()
-    {
-        // Listen for search results from backend
-        chrome.extension.onConnect.addListener(function(port) {
-            if(port.name != "uiToBackend") {
-                console.log("Invalid port name: " + port.name);
-                return;
-            }
+  if (localStorage.getItem('uivisits') < 4) {
+    $('#welcomesearchbox').append(
+      '<div id="tiparea"><p><b>Tip:</b> Use "*". For example, to search for words beginning with "mar", type "mar*".</div>'
+    );
+  }
+  if (localStorage.getItem('uivisits') == 3) {
+    $('#tiparea').delay(3000).fadeOut('slow');
+  }
 
-            port.onMessage.addListener(function(result) {
-                processSearchResult(result);
-            });
-        });
+  $('#searchbox').focus();
+  $('#searchbox').keyup(function(e) {
+    if (e.keyCode == 13) {
+      leavePage('#welcomepage');
+    }
+  });
 
-        localStorage['uivisits']++;
+  $('#searchbutton').click(function() {
+    leavePage('#welcomepage');
+  });
+});
 
-        /* stop showing tip after 2 UI visits */
-        if(localStorage['uivisits'] < 4)
-            $('#welcomesearchbox').append(
-                '<div id="tiparea"><p><b>Tip:</b> Use the "*". ' +
-                    'For example, to search for words beginning ' +
-                    'with "mar", simply type "mar*".</div>');
-
-        if(localStorage['uivisits'] == 3)
-            $('#tiparea').delay(3000).fadeOut(
-                'slow',
-                function () {});
-
-        $('#searchbox').focus();
-        $('#searchbox').keyup(function(e) {
-	        if(e.keyCode == 13) {
-		        leavePage('#welcomepage');
-	        }
-        });
-    });
-
-// Send request to backend for displaying a cached bookmark page
 function requestCachedPage(id) {
-    chrome.extension.sendRequest(
-        {method: 'cached', bookmarkid: id},
-        function() {});
+  chrome.runtime.sendMessage({ method: 'cached', bookmarkid: id });
 }
 
 var resultspagename = '#resultspage';
 
 var results_page_top_html =
-    '<div id="topsearch">' +
-    '<table><tr>' +
-    '<th id = "thlogo"><img src="images/logotext-results.png"/></th>' +
-    '<!-- search box -->' +
-    '<th id = "thsearchbox"><input type="text" id="searchbox" class="searchbox"/>' +
-    '<!-- search button -->' +
-    '<button type="button" id="searchbutton" class="searchbutton">Search</button></th>' +
-    '</tr></table>' +
-    '</div>';
+  '<div id="topsearch">' +
+  '<table><tr>' +
+  '<th id="thlogo"><img src="images/logotext-results.png"/></th>' +
+  '<th id="thsearchbox"><input type="text" id="searchbox" class="searchbox"/>' +
+  '<button type="button" id="searchbutton" class="searchbutton">Search</button></th>' +
+  '</tr></table>' +
+  '</div>';
 
-// Take keywords from text box and send to
-// backend for searching.
 function doSearch(searchwords) {
+  $('body').append('<div id="resultspage"></div>');
+  $('#resultspage').append(results_page_top_html);
+  $('#resultspage').append('<div id="resultspagebtm"></div>');
+  $('#resultspage #searchbutton').click(function() {
+    leavePage(resultspagename);
+  });
 
-    $('body').append('<div id = "resultspage"></div>');
-    $('#resultspage').append(results_page_top_html);
-    $('#resultspage').append(
-    '<div id="resultspagebtm"></div>');
-    $('#resultspage #searchbutton').click(function(){
-        leavePage(resultspagename);
-    });
-
-    chrome.extension.sendRequest(
-        {method: 'search', keywords: searchwords},
-        function() {});
+  chrome.runtime.sendMessage({ method: 'search', keywords: searchwords });
 }
 
-function leavePage(pagename)
-{
-    var searchwords = $('#searchbox').val();
-    $(pagename).remove();
-    $('body').css('cursor', 'wait');
-    doSearch(searchwords);
+function leavePage(pagename) {
+  var searchwords = $('#searchbox').val();
+  $(pagename).remove();
+  $('body').css('cursor', 'wait');
+  doSearch(searchwords );
 }
 
-// Each search result will be delivered here.
-// result object has fields,
-//   result.url: page URL
-//   result.text: formatted page text, or URL
-//   result.title: page title
-//   result.matchType: 'title', 'url', or 'page'
-//     If the title matched, then result.text will be
-//     empty. if URL matched, then result.text will be
-//     a formatted URL (keyword is in bold face), if
-//     page text matched, then result.text will contain
-//     textual context.
 function processSearchResult(result) {
-    if(result.matchType == "DONE") {
-        $('body').css('cursor', 'auto');
-        // search error?
-        if(result.error) {
-            resultString = result.error;
-        } else {
-            resultString = "&nbsp;";
-        }
-
-        $('#searchbox').focus();
-        $('#searchbox').keyup(function(e) {
-	        if(e.keyCode == 13) {
-		        leavePage('#resultspage');
-	        }
-        });
-
-    } else {
-
-    if(result.img == "")
-    result.img = '<img src="" alt="No Image Available" ' +
-    'width=400 />';
-    else
-    result.img = '<img src="' + result.img +
-    '" width=400 />';
-
-        resultString =
-            '<a href="' + result.url + '" target="_blank">' + result.title +'</a>' +
-            '<br/>' + result.text + '<br/>' +
-            '<span class="resulturl">' + result.url + '</span>&nbsp;' +
-            '<a href="#" class="resultactions">(Offline Version)</a><br/>' + result.img + '<p><br/>';
-    }
-
-    $('#resultspagebtm').append(resultString);
-    $('#resultspagebtm .resultactions').click(function(){
-        requestCachedPage(result.id);
+  if (result.matchType == "DONE") {
+    $('body').css('cursor', 'auto');
+    let resultString = result.error ? result.error : " ";
+    $('#searchbox').focus();
+    $('#searchbox').keyup(function(e) {
+      if (e.keyCode == 13) {
+        leavePage('#resultspage');
+      }
     });
+    $('#resultspagebtm').append(resultString);
+  } else {
+    let img = result.img == "" ? '<img src="" alt="No Image Available" width=400 />' : '<img src="' + result.img + '" width=400 />';
+    let resultString =
+      '<a href="' + result.url + '" target="_blank">' + result.title + '</a>' +
+      '<br/>' + result.text + '<br/>' +
+      '<span class="resulturl">' + result.url + '</span> ' +
+      '<a href="#" class="resultactions">(Offline Version)</a><br/>' + img + '<p><br/>';
+    $('#resultspagebtm').append(resultString);
+    $('#resultspagebtm .resultactions').last().click(function() {
+      requestCachedPage(result.id);
+    });
+  }
 }
-
-$('#searchbutton').click(function(){
-    leavePage('#welcomepage');
-});
